2faba874f6e5ed4db470e99cbd9e4cb96ddc4d5b
nguyenmanhtuan2k14-code <nguyenmanhtuan2k14@gmail.com>
2026-03-19 17:41:34 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running");
+});
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
